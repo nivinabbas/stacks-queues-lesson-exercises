0ec7e52b1c9fd713834e41c8125d91ba5c29b869
nivinabbas <nivin.abbas1@gmail.com>
2021-12-07 23:43:19 +0200

    add class stack and methods

--- src/Stack.js
+++ src/Stack.js
@@ -1,17 +1,30 @@
-/* Write your class below */
+class Stack {
+    constructor() {
+        this.stack = []
+        this.length = 0
+    }
 
-
-
-
-
-
-
-
-//You can paste the test code from the lesson below to test your solution
-
-
-
-
-
-/* Do not remove the exports below */
+    push(x) {
+        this.stack[this.length] = x
+        this.length++
+    }
+    
+    isEmpty() {
+        return this.length === 0
+    }
+    peek() {
+        return this.isEmpty() ? null : this.stack[this.length - 1]
+    }
+    
+    pop() {
+        if (this.isEmpty()) {
+            return null
+        }
+        this.length--
+        return delete this.stack[this.length]
+    }
+    print() {
+        console.log(this.stack)
+    }
+}
 module.exports = Stack
